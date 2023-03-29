54ef85a01f27ef20631a22823615963930a536ef
Tom-Stranger <115110026+Glutey@users.noreply.github.com>
2023-03-29 21:54:38 +0100

    Update Rules.js
    Amended the rules modal, to include information about how to play the game and what the various questions sets are about using the abbreviations.

--- word-game/src/components/Rules.js
+++ word-game/src/components/Rules.js
@@ -45,28 +45,43 @@ function GameRulesModal() {
   // );
   return (
     <div>
-      <button class="js-modal-trigger" data-target="modal-js-example">Open Game Rules</button>
+      <button class="js-modal-trigger" data-target="modal-js-example">How to Play</button> 
 
       {showModal && (
         <div id="modal-js-example" className="modal">
           <div className="modal-content">
             <h2>How to Play</h2>
-
-            <ul>
-              <li>Choose number of questions, game type and difficulty level.</li>
-              <li>Choose whether you want a timed quiz or not.</li>
+     
+             <ul>
+              <li>Select the number of questions, 
+                game type, race agaisnt the clock
+                and difficulty level you wish to play.</li>
               <li>
-                In the game, three words will appear on the screen. Below them,
-                there will be two selectable word options. You must choose the option
-                that relates to all three words.
+                There are several question sets to choose from.
+
+                GMAT - A test for business school admission that assesses analytical, writing, quantitative, verbal, and reading skills.
+
+                TOEFL - A test to assess the English language skills of non-native English speakers who want to study or work in an English-speaking country.
+
+                SAT - A test for college admission in the United States that assesses critical reading, mathematics, and writing skills.
+
+                GRE - A test for graduate school admission that assesses analytical writing, verbal reasoning, and quantitative reasoning skills.
+
+                IELTS - A test to assess the English language proficiency of non-native English speakers who want to study or work in an English-speaking country.
+              </li>
+              <li>
+                3 words will appear on screen. 
+                You must select the word from the menu that is most like the others.
               </li>
               <li>
                 Example: 
-                    Tree, Branch, Root.
+
+                    Tree, Vegetable, Groceries.
+
                         Plant - dog
+
                 Correct answer would be Plant.
               </li>
-              <li>For each correct answer, you will receive 1 point.</li>
               <li>Try to get as many questions right as possible to climb the leaderboard!</li>
             </ul>
 
