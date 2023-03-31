d28a0f66d91fd32a9067cb8b30efb794ebc7f71a
Eugene Akinbamini <eugeneakinson@gmail.com>
2023-03-31 10:22:04 +0100

    readme template

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import RenderQuestions from './components/RenderQuestions';
-import WelcomePage from './components/WelcomePage';
-import Rules from "./components/Rules";
-import TimelessQuiz from './components/TimelessQuiz';
-import GameSettings from './components/GameSettings';
+
 
 
 
